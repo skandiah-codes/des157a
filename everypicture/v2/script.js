bc9462ce6aec6fcfedd9d7cb1c5a22dd887103e6
Shampave Kandiah <skandiah@ucdavis.edu>
2025-11-17 12:57:21 -0800

    final every picture thank god

--- everypicture/v2/script.js
+++ everypicture/v2/script.js
@@ -1,28 +1,69 @@
 (function(){
     'use strict';
     console.log('reading js');
-window.addEventListener('load', function () {
-    'use strict';
-    const sections = document.querySelectorAll('section');
-    const headerP = document.querySelector('header p');
-    let sectionTops = [];
-    let pagetop;
-    let counter = 1;
-    let prevCounter = 1;
-    let doneResizing;
-    sections.forEach(function (eachSection) {
-    sectionTops.push(Math.floor(eachSection.getBoundingClientRect().top) + window.scrollY);
+
+    window.addEventListener('load', function () { //from the example, changes color
+        'use strict';
+
+        const sections = document.querySelectorAll('section');
+
+        let sectionTops = [];
+        let pagetop;
+        let counter = 0;
+        let prevCounter = 1;
+        sections.forEach(function (eachSection) {
+            sectionTops.push(
+                Math.floor(eachSection.getBoundingClientRect().top) + window.scrollY
+            );
+        });
+
+        sectionTops.push(Number.MAX_SAFE_INTEGER);
+
+        window.addEventListener('scroll', function () {
+            pagetop = window.scrollY + 100;
+        });
+
+
+        window.addEventListener('scroll', function () {
+            pagetop = window.scrollY + 100;
+
+            if (pagetop > sectionTops[counter]) {
+                counter++;
+            }
+
+ 
+            else if (counter > 1 && pagetop < sectionTops[counter - 1]) {
+                counter--;
+            }
+
+
+            if (counter !== prevCounter) {
+                document.querySelector('body').className = 'bgcolor' + counter;
+                prevCounter = counter;
+            }
+        });
+
+    });
+
+    document.addEventListener("DOMContentLoaded", function () {
+
+    var items = document.querySelectorAll(".fade-target"); //inspired by example of changing sections text sizes, instead has the text fade in
+
+    var observer = new IntersectionObserver(function (entries, observer) {
+        entries.forEach(function (entry) {
+            if (entry.isIntersecting) {
+                entry.target.classList.add("fade-in");
+                observer.unobserve(entry.target)
+            }
+        });
+    }, {
+        threshold: 0.2
+    });
+
+    items.forEach(function (item) {
+        observer.observe(item);
+    });
 });
-
-console.log(sectionTops);
-});
-
-if (counter != prevCounter) {
-    const myStyle = `bgcolor${counter}`;
-    document.querySelector('body').className = myStyle;
-    prevCounter = counter;
-}
-
 
 
 })();
